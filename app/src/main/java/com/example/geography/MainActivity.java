290fa0a278e767b9e06f84bc0a13e02d9dcdff2e
QuFlax <daniilkravcenko2014@gmail.com>
2021-07-28 23:06:16 +0300

    1

---
+++ app/src/main/java/com/example/geography/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.geography;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.geography.databinding.MainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+    private AppBarConfiguration mAppBarConfiguration;
+    private MainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = MainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_info, R.id.nav_test
+        ).setDrawerLayout(drawer).build();
+        navController = Navigation.findNavController(this, R.id.host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+}
---
+++ app/src/main/java/com/example/geography/fragments/InfoFragment.java
@@ -0,0 +1,32 @@
+package com.example.geography.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.geography.databinding.FragmentInfoBinding;
+
+public class InfoFragment extends Fragment {
+    private FragmentInfoBinding binding;
+
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentInfoBinding.inflate(inflater, container, false);
+        return onCreate(binding.getRoot());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    private View onCreate(View root) {
+
+
+
+        return root;
+    }
+}
