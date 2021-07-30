7ec0c11a61abf7271075e0b8ce78e6f5c706ff67
QuFlax <daniilkravcenko2014@gmail.com>
2021-07-30 20:08:22 +0300

    2222

--- app/src/main/java/com/example/geography/MainActivity.java
+++ app/src/main/java/com/example/geography/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.geography;
 
 import android.os.Bundle;
-import android.view.Menu;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -13,11 +12,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.geography.databinding.MainBinding;
+import static com.example.geography.TestQ.*;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
     private AppBarConfiguration mAppBarConfiguration;
     private MainBinding binding;
+    public static int qIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
         binding = MainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        randomQ();
 
         setSupportActionBar(binding.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
@@ -40,5 +45,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    private void randomQ() {
+        final Random r = new Random();
+        final int len = q.length;
+        for (int i = 0; i < len; i++) {
+            final String[] s = q[i];
+            for (int j = q[i].length - 1; j > 0; j--) { // Рандом ответов
+                final int jMO = j-1;
+                final int index = (jMO != 0) ? r.nextInt(jMO)+1 : 1;
+                final boolean jQAns = j == qAns[i];
+                if (jQAns || index == qAns[i])
+                    qAns[i] = jQAns ? index : j;
+                String tmp = s[index]; s[index] = s[j]; s[j] = tmp;
+            }
+        }
+        for (int i = len - 1; i > 0; i--) { // Рандом вопросов
+            final int index = r.nextInt(i);
+            int swapQAns = qAns[index]; qAns[index] = qAns[i]; qAns[i] = swapQAns;
+            String[] tmp = q[index]; q[index] = q[i]; q[i] = tmp;
+        }
     }
 }
--- app/src/main/java/com/example/geography/fragments/TestFragment.java
+++ app/src/main/java/com/example/geography/fragments/TestFragment.java
@@ -1,37 +1,62 @@
 package com.example.geography.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.geography.MainActivity;
 import com.example.geography.databinding.FragmentTestBinding;
 
-import java.util.Random;
+import static com.example.geography.TestQ.*;
+import static com.example.geography.MainActivity.qIndex;
+import static com.example.geography.TestQ.qAns;
 
 public class TestFragment extends Fragment {
-    public static final String[][] q = {
-            {"З чого складається водна оболонка Землі - гідросфера?",
-                    "з вод суші і Світового океану",
-                    "з вод суші, Світового океану і води в атмосфері - правильно",
-                    "з річок, озер, морів, боліт, ставків",
-                    "тільки з вод суші"},
-            {"Як називаються ділянки суші, оточені з трьох сторін водою, а з четвертого - з'єднані з нею?",
-                    "материки","архіпелаги","півострова - правильно","острова"},
-            {"Яка частина гідросфери не відображена на картах?",
-                    "річки","моря","озера","підземні води - правильно"},
-            {"Як називається безперервний процес переміщення води зі Світового океану на сушу і назад?",
-                    "Світовий океан","течії","водна спільнота","Світової кругообіг води - правильно"},
-            {"Яке харчування має більшість річок Росії?",
-                    "снігове - правильно","дощове","льодовикове","змішане"}
-    };
     private FragmentTestBinding binding;
+    private CheckBox[] checkBoxes;
 
-    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+    @SuppressLint("SetTextI18n")
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentTestBinding.inflate(inflater, container, false);
-        return onCreate(binding.getRoot());
+        checkBoxes = new CheckBox[]{binding.checkBox1, binding.checkBox2, binding.checkBox3, binding.checkBox4};
+        setQ(qIndex);
+        binding.back.setOnClickListener((e)->{
+            setAns();
+            if (qIndex != 0)
+                setQ(--qIndex);
+        });
+        binding.next.setOnClickListener((e)->{
+            setAns();
+            if (qIndex != (q.length-1))
+                setQ(++qIndex);
+            else
+                binding.finish.setVisibility(View.VISIBLE);
+        });
+        binding.finish.setOnClickListener((e)-> setAns());
+        return binding.getRoot();
+    }
+
+    private void setQ(int i) {
+        binding.qText.setText(q[qIndex][0]);
+        binding.checkBox1.setText(q[i][1]);
+        binding.checkBox2.setText(q[i][2]);
+        binding.checkBox3.setText(q[i][3]);
+        binding.checkBox4.setText(q[i][4]);
+        binding.finish.setVisibility(View.INVISIBLE);
+    }
+
+    private void setAns() {
+        for (int i = 0; i < checkBoxes.length; i++)
+            ans[qIndex][i] = (i == (qAns[qIndex]-1)) == checkBoxes[i].isChecked();
+    }
+
+    private String TF(int i) {
+        return ans[qIndex][i]?"T":"F";
     }
 
     @Override
@@ -39,32 +64,4 @@ public class TestFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-    private View onCreate(View root) {
-        Random r = new Random();
-        for (String[] s: q) random(s, true);
-        random(q, false);
-        binding.qText.setText(q[0][0]);
-        binding.checkBox1.setText(q[0][1]);
-        binding.checkBox2.setText(q[0][2]);
-        binding.checkBox3.setText(q[0][3]);
-        binding.checkBox4.setText(q[0][4]);
-        return root;
-    }
-
-    private void random(Object[] s, boolean generateAnswers) {
-        final Random r = new Random();
-        for (int i = s.length - 1; i > 0; i--) { // Рандом ответов
-            final int index = generateAnswers ? ((i-1)!=0 ? r.nextInt(i-1)+1 : 1) : r.nextInt(i);
-            Object tmp = s[index]; s[index] = s[i]; s[i] = tmp;
-        }
-    }
-
-    /*
-        <item>Який океан вважається найбільшим?</item>
-        <item>Яке озеро наполовину складається з прісної води, а наполовину - з солоної?</item>
-        <item>Вкажіть штучну водойму:</item>
-        <item>В яке море впадає річка Волга?</item>
-        <item>Яка течія починається біля берегів Північної Америки і має північно-східний напрямок?</item>
-     */
 }
